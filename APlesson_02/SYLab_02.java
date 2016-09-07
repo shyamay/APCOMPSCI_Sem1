31cba87079385100bfc4d151598c45fbf0277c8d
shyamay <shyamay@gmail.com>
2016-09-07 11:45:45 -0700

    Lesson_02

---
+++ APlesson_02/SYLab_02.java
@@ -0,0 +1,30 @@
+public class SYLab_02
+{
+	public static void main(String[]args)
+	{
+		int l = 21;
+		int w = 36;
+		int recArea = l * w;
+		
+		System.out.println("21 multiplied by 36 is" +" "+ recArea);
+		
+		String name = "Shyama Yallapragada";
+		String address = "3527 Meadows Lane";
+		String city = "San Diego";
+		String zip = "92130";
+		
+		System.out.println(name + "\n" + address + "\n" + city + "\n" + zip);
+		
+		int a = 15;
+		int b = 17;
+		int c = 19;
+		int SurfaceArea = 2 * a * b + 2 * a * c + 2 * b * c;
+		
+		System.out.println("The surface area of your rectangle is" +" "+ SurfaceArea);
+		
+		
+		
+		
+	}
+	
+}
